592d3b6f7a77dc29114f012f7e1630c7158e19e9
ferib <lopke123@live.nl>
2019-06-01 14:59:58 +0200

    Added some comment

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace PoC_MultiThread
     class Program
     {
         private static int IntervalTimeMs = 50;
-        private static int ThreadCount = 21;
+        private static int ThreadCount = 20;
         private static DateTime StartTime = DateTime.Now;
         private static Thread[] ScanningThreads = new Thread[ThreadCount];
         private static DateTime[] LastThreadActivity = new DateTime[ThreadCount];
@@ -14,6 +14,7 @@ namespace PoC_MultiThread
         private static int DecreaseValue = 0;
         static void Main(string[] args)
         {
+            //ASCII art
             Console.WriteLine("");
             Console.WriteLine("   ____ ___ ___  _ ___  ");
             Console.WriteLine("  |  __| __| _ \\| | _ \\ ");
@@ -29,9 +30,11 @@ namespace PoC_MultiThread
                 Console.WriteLine("   ╠══════════════════════════╣");
                 for (int i = 1; i < ThreadResult.Length; i++)
                 {
+                    //Print last threads result and date of update for each thread
                     Console.WriteLine("   ║ " + LastThreadActivity[i].ToString("HH:mm:ss.FFF").PadRight(11) + " - " +  ThreadResult[i].ToString().PadLeft(9) + " ║");
                 }
                 Console.WriteLine("   ╚══════════════════════════╝");
+                //sleep to avoid screen from refreshing to fast
                 Thread.Sleep(50);
                 Console.Clear();
             }
@@ -42,6 +45,7 @@ namespace PoC_MultiThread
 
         private static void StartThreads()
         {
+            //start all threads at once, asign current DateTime
             DateTime StartTime = DateTime.Now;
             for (int i = 0; i < ScanningThreads.Length; i++)
             {
@@ -54,6 +58,7 @@ namespace PoC_MultiThread
         }
         private static void KillThreads()
         {
+            //TODO: use cancellation token to close threads properly
             for (int i = 0; i < ScanningThreads.Length; i++)
             {
                 if(ScanningThreads[i].IsAlive)
@@ -65,14 +70,16 @@ namespace PoC_MultiThread
         {
             while (true)
             {
-                if(LastThreadActivity[Thread.CurrentThread.ManagedThreadId - DecreaseValue].AddMilliseconds((ThreadCount-1)*IntervalTimeMs) < DateTime.Now)
+                if(LastThreadActivity[Thread.CurrentThread.ManagedThreadId - DecreaseValue].AddMilliseconds((ThreadCount)*IntervalTimeMs) < DateTime.Now)
                 {
-                    LastThreadActivity[Thread.CurrentThread.ManagedThreadId - DecreaseValue] = LastThreadActivity[Thread.CurrentThread.ManagedThreadId - DecreaseValue].AddMilliseconds((ThreadCount - 1) * IntervalTimeMs);
+                    LastThreadActivity[Thread.CurrentThread.ManagedThreadId - DecreaseValue] = LastThreadActivity[Thread.CurrentThread.ManagedThreadId - DecreaseValue].AddMilliseconds((ThreadCount) * IntervalTimeMs);
+                    //Generate random number to show visible changes
                     Random rnd = new Random();
                     ThreadResult[Thread.CurrentThread.ManagedThreadId - DecreaseValue] = rnd.Next(1000, 10000);
                 }
                 else
                 {
+                    //sleep to prevent CPU for getting raped
                     Thread.Sleep(2);
                 }
             }
